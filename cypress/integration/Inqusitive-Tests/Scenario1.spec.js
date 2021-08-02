8bde4e81ff95a49486ac741f0e7115ae868fac79
VibhaJo <88295247+VibhaJo@users.noreply.github.com>
2021-08-02 12:42:12 +1000

    Delete cypress/integration/Inqusitive-Tests directory

--- cypress/integration/Inqusitive-Tests/Scenario1.spec.js
+++
@@ -1,8 +0,0 @@
-describe('TestForScenario-1', () => {
-    
-// Checking for Scenario 1 - Verifying the Text 'Inquisitive Differentiation' is present.
-    it('CheckforText-InqDiff', () => {
-        cy.visit('https://www.inquisitive.com/about')
-        cy.contains('Inquisitive Differentiation').scrollIntoView().should('be.visible')
-    })  
-})
